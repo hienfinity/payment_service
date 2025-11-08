18999ac2ef9ef60af287765a350a737990f82283
hiend8888 <hiendo2000@yahoo.de>
2025-11-08 19:11:29 +0100

    update mock_data.py

--- payment_service/mock_data.py
+++ payment_service/mock_data.py
@@ -1,5 +1,5 @@
 mock_accounts = [
-    {"customer_id": "CUST-45823", "name": "Andreas Heberle", "balance": 300.0},
-    {"customer_id": "CUST-11111", "name": "Lukas Wagner", "balance": 50.0},
-    {"customer_id": "CUST-22222", "name": "Marie Becker", "balance": 1500.0}
+    {"customer_id": "CUST-11111", "name": "Andreas Heberle", "balance": 300.0},
+    {"customer_id": "CUST-22222", "name": "Lukas Wagner", "balance": 50.0},
+    {"customer_id": "CUST-33333", "name": "Marie Becker", "balance": 1500.0}
 ]
